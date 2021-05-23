5432aa8c4bea864b33eee854b77c526e7b18e755
aphasia0 <tony.dream@gmail.com>
2021-05-23 16:17:04 +0200

    first commit

--- src/app/joke/joke.component.ts
+++ src/app/joke/joke.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Joke } from './joke';
 import { FormBuilder, Validators } from '@angular/forms';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-joke',
@@ -14,25 +15,36 @@ export class JokeComponent implements OnInit {
     title: [null, Validators.required],
     content: [null, Validators.required],
   });
+  $jokes: Observable<Joke[]> = new Observable();
+  $joke: Observable<Joke> = new Observable();
 
   constructor(private fb: FormBuilder, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.onClick();
+  }
 
   onSubmit(): void {
     this.http
       .post('http://localhost:3004/barzellete', {
-        title: this.jokeForm.value,
+        ...this.jokeForm.value,
       })
       .subscribe(console.log);
   }
-  $jokes: Observable<Joke> = new Observable();
-
-  ngOnInit(): void {
-    this.$jokes = this.http.get<Joke>('http://localhost:3004/barzellete');
-  }
 
   onClick(): void {
-    this.$jokes.pipe();
-    8;
-    999999;
+    this.$jokes = this.http.get<Joke[]>('http://localhost:3004/barzellete');
+    this.$joke = this.$jokes.pipe(
+      switchMap((value) => {
+        let id = Math.round(Math.random() * (value.length - 1));
+        return this.http.get<Joke>('http://localhost:3004/barzellete/' + id);
+      })
+    );
+    // this.$jokes.subscribe((value) => {
+    //   let id = Math.round(Math.random() * value.length - 1);
+    //   this.http
+    //     .get<Joke>('http://localhost:3004/barzellete/' + id)
+    //     .subscribe((value1) => (this.$joke = of(value1)));
+    // });
   }
 }
