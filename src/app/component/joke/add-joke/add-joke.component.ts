91bbc2b8d8cc8f02bb764098af71b0ca92a8336d
aphasia0 <tony.dream@gmail.com>
2021-05-30 11:57:03 +0200

    222

---
+++ src/app/component/joke/add-joke/add-joke.component.ts
@@ -0,0 +1,10 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-add-joke',
+  templateUrl: './add-joke.component.html',
+  styleUrls: ['./add-joke.component.scss'],
+})
+export class AddJokeComponent implements OnInit {
+  ngOnInit(): void {}
+}
---
+++ src/app/component/joke/view-joke/view-joke.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-joke',
+  templateUrl: './view-joke.component.html',
+  styleUrls: ['./view-joke.component.scss']
+})
+export class ViewJokeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
