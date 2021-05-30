3a471803fc3d23e8ee210c80d6287540786dcc54
aphasia0 <tony.dream@gmail.com>
2021-05-30 12:33:10 +0200

    222

--- src/app/component/joke/add-joke/add-joke.component.ts
+++ src/app/component/joke/add-joke/add-joke.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Joke } from '../../../joke';
 
 @Component({
   selector: 'app-add-joke',
@@ -6,5 +8,19 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./add-joke.component.scss'],
 })
 export class AddJokeComponent implements OnInit {
+  @Output()
+  submitClick: EventEmitter<Joke> = new EventEmitter();
+
+  jokeForm = this.fb.group({
+    title: [null, Validators.required],
+    content: [null, Validators.required],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
   ngOnInit(): void {}
+
+  onSubmit() {
+    this.submitClick.emit(this.jokeForm.value);
+  }
 }
--- src/app/component/joke/view-joke/view-joke.component.ts
+++ src/app/component/joke/view-joke/view-joke.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Joke } from '../../../joke';
 
 @Component({
   selector: 'app-view-joke',
   templateUrl: './view-joke.component.html',
-  styleUrls: ['./view-joke.component.scss']
+  styleUrls: ['./view-joke.component.scss'],
 })
 export class ViewJokeComponent implements OnInit {
+  @Input()
+  joke: Joke | undefined;
 
-  constructor() { }
+  @Output()
+  deleteClick: EventEmitter<void> = new EventEmitter<void>();
 
-  ngOnInit(): void {
+  @Output()
+  likeClick: EventEmitter<void> = new EventEmitter<void>();
+
+  catUrl: string = "url('https://placekitten.com/g/200/3";
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  delete() {
+    this.deleteClick.emit();
   }
 
+  like() {
+    this.likeClick.emit();
+  }
 }
